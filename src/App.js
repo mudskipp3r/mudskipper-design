da5e54d13e779f3e1c22f6d2302cd6e8463bb009
Mudskipper <dawson@mudskipper.design>
2021-11-18 09:41:51 +1100

    Update Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Appa from './images/Appa.jpg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProjectScreen from './screens/ProjectScreen';
 
 function App() {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route exact path="/" component={<HomeScreen />} />
                 <Route path="/HIA" component={<ProjectScreen />} />
-            </Switch>
+            </Routes>
         </Router>
     );
 }

5519a2dd3ed2379c54d03199214bcab117a032d5
Mudskipper <dawson@mudskipper.design>
2022-05-04 10:27:04 +1000

    Remove footer component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { darkTheme, lightTheme } from './themes';
 import SplashScreen from './screens/SplashScreen';
 import styled from '@emotion/styled';
-import Footer from './components/Footer/Footer';
 import TestScreen from './screens/TestScreen';
 import SidebarNavigation from './components/PrimaryNavigation/SidebarNavigation';
 
